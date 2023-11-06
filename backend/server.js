0169535dd09b80f52a165bc481b950daf6d5bf84
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-06 15:36:46 +0200

    Server <> Used .env PORT Variable

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // Required packages
+require('dotenv').config();
 const express = require('express');
 
 // Created an express app
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
 });
 
 // Listen for requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
 	console.log('Listening on port 4000');
 });
