79d561c32b0d9ad7f4e6e3c54b70176da93a3b7d
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-06 18:46:44 +0200

    Server <> Added Single Workout Details Route

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -5,8 +5,14 @@ const express = require('express');
 const router = express.Router();
 
 // Setup routes
+// Get all workouts
 router.get('/', (req, res) => {
 	res.json({ message: 'Get all workouts' });
+});
+
+// Get single workout details
+router.get('/:id', (req, res) => {
+	res.json({ message: 'Get a single workout' });
 });
 
 // Export routes
