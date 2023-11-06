e2a8e1a970bbb1cadcb6eacdba81600045240ba9
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-06 18:44:20 +0200

    Server <> Added Home Route

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -3,3 +3,11 @@ const express = require('express');
 
 // Setup express router
 const router = express.Router();
+
+// Setup routes
+router.get('/', (req, res) => {
+	res.json({ message: 'Get all workouts' });
+});
+
+// Export routes
+module.exports = router;
