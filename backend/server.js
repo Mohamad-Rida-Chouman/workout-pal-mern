13e40ec31ab6edd6d19605f38916be6bb83f8d2b
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-06 15:33:15 +0200

    Server <> Added Homepage Route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const express = require('express');
 // Created an express app
 const app = express();
 
+// Routes
+app.get('/', (req, res) => {
+	res.json({ message: 'Welcome to the app' });
+});
+
 // Listen for requests
 app.listen(4000, () => {
 	console.log('Listening on port 4000');
