30c440a9b2f3e9daf63c7f7d899cf745c80f5f22
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-06 12:46:15 +0200

    Server <> Added Listen to Requests on Port 4000

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,8 @@ const express = require('express');
 
 // Created an express app
 const app = express();
+
+// Listen for requests
+app.listen(4000, () => {
+	console.log('Listening on port 4000');
+});
