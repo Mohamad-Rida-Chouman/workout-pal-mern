089c08b25a037bfb1249c1ecfd280d3f78625a92
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-08 15:14:08 +0200

    Client <> Setup Routes Boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+// Import packages
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages and components
+import Home from './pages/Home';
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<div className="pages">
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
