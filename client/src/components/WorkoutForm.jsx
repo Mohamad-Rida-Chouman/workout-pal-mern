834166ca997570603a899650a8316da9a6f556db
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-08 17:09:41 +0200

    Client <> Added Form to Fill in Workout Form Component

--- client/src/components/WorkoutForm.jsx
+++ client/src/components/WorkoutForm.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const WorkoutForm = () => {
-    const [title, setTitle] = useState('')
-    const [load, setLoad] = useState('')
-    const [reps, setReps] = useState('')
-    return (  );
-}
- 
+	const [title, setTitle] = useState('');
+	const [load, setLoad] = useState('');
+	const [reps, setReps] = useState('');
+	return (
+		<form className="create">
+			<h3>Add a Workout</h3>
+			<label> Exercise Title:</label>
+			<input
+				type="text"
+				onChange={(e) => setTitle(e.target.value)}
+				value={title}
+			/>
+			<label> Load (in Kg):</label>
+			<input
+				type="number"
+				onChange={(e) => setLoad(e.target.value)}
+				value={load}
+			/>
+			<label> Reps:</label>
+			<input
+				type="number"
+				onChange={(e) => setReps(e.target.value)}
+				value={reps}
+			/>
+			<button>Add Workout</button>
+		</form>
+	);
+};
+
 export default WorkoutForm;
