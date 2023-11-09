a6f93124f16eeea07240211ba3512b9269bda942
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-09 06:29:31 +0200

    Client <> Defined Workouts Reducer Function With Case SET_WORkOUT

--- client/src/context/WorkoutsContext.js
+++ client/src/context/WorkoutsContext.js
@@ -4,9 +4,17 @@ import { createContext, useReducer } from 'react';
 // Create variable to contain create context
 export const WorkoutsContext = createContext();
 
+export const workoutsReducer = (state, action) => {
+	switch (action.type) {
+		case 'SET_WORKOUTS':
+			return { workouts: action.payloads };
+	}
+};
+
 export const WorkoutsContextProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(workoutsReducer, {
 		workouts: null,
 	});
+	dispatch({ type: 'SET_WORKOUTS' }, payload[({}, {})]);
 	return <WorkoutsContext.Provider>{children}</WorkoutsContext.Provider>;
 };
