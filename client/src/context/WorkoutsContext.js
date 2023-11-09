f563792046dee87a5dec1c5bba39674b87e22e9a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-11-09 05:45:32 +0200

    Client <> Initiated useReduce Hook

--- client/src/context/WorkoutsContext.js
+++ client/src/context/WorkoutsContext.js
@@ -1,9 +1,12 @@
 // Import Packages
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 // Create variable to contain create context
 export const WorkoutsContext = createContext();
 
 export const WorkoutsContextProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(workoutsReducer, {
+		workouts: null,
+	});
 	return <WorkoutsContext.Provider>{children}</WorkoutsContext.Provider>;
 };
